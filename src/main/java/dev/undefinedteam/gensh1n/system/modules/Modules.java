127a6cb7909835fc4c63c5d6a1488245b224d100
ImWuMie <110892807+ImWuMie@users.noreply.github.com>
2024-11-23 13:53:43 +0800

    clean

--- src/main/java/dev/undefinedteam/gensh1n/system/modules/Modules.java
+++ src/main/java/dev/undefinedteam/gensh1n/system/modules/Modules.java
@@ -77,9 +77,7 @@ public class Modules extends System<Modules> {
 
     private void initCombat() {
         add(new KillAura());
-//        add(new Velocity());
         add(new InfiniteAura());
-//        add(new TrowableAura());
     }
 
     private void initPlayer() {
@@ -88,7 +86,6 @@ public class Modules extends System<Modules> {
         add(new AutoTool());
         add(new Stealer());
         add(new Blink());
-//        add(new Blink2());
         add(new NoFall());
     }
 
@@ -96,12 +93,10 @@ public class Modules extends System<Modules> {
         add(new AutoClip());
         add(new BoatJump());
         add(new AntiWeb());
-//        add(new GuiMove());
         add(new MoveFix());
         add(new Sprint());
         add(new NoPush());
         add(new NoSlow());
-//        add(new Speed());
     }
 
     private void initRender() {
@@ -109,8 +104,6 @@ public class Modules extends System<Modules> {
         add(new NameProtect());
         add(new ItemPhysics());
         add(new FullBright());
-//        add(new KillEffect());
-//        add(new Particles());
         add(new HitSound());
         add(new DickESP());
         add(new ESP());
@@ -119,20 +112,16 @@ public class Modules extends System<Modules> {
     private void initWorld() {
         add(new PistonBreaker());
         add(new Scaffold());
-//        add(new ScaffoldTest());
         add(new InstaNuker());
-        //add(new ScaffoldV2());
     }
 
     private void initMisc() {
         add(new SuperSpammer());
-//        add(new BoatSpin());
         add(new FakePlayer());
         add(new AntiStaff());
         add(new AutoPlay());
         add(new Protocol());
         add(new AutoCalc());
-//        add(new Disabler());
         add(new AntiBot());
         add(new Teams());
         add(new Debug());
